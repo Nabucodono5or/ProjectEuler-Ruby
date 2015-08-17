4ebf7e5592a0d22efd1f6244d408cdb4ce4dd174
Jefferson Abreu <nabucodonosor310@gmail.com>
2015-08-17 10:44:47 -0300

    primeiro problema

---
+++ ProblemOne/Multiple.rb
@@ -0,0 +1,12 @@
+def problem_one
+	total = 0;
+	max = 1000
+
+	for numero in (1..max-1)
+		if numero%3 ==0 || numero%5 ==0
+			puts numero
+			total += numero
+		end	
+	end		
+	puts "A soma dos multiplos de 3 e 5 abaixo de #{max} é #{total}" 
+end	
---
+++ executadorEuler.rb
@@ -0,0 +1,4 @@
+puts "execute qualquer um dos seguintes comandos para obter a resposta do respectivo problema:
+problem_one"
+
+require File.expand_path('ProblemOne/Multiple')
